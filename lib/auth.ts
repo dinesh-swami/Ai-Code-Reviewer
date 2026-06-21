0269ff1ca05d1d77bb1b392e276a458aecf8539b
Dinesh Swami <dinesh907989@gmail.com>
2026-06-21 15:33:45 +0530

    better auth setup done.

--- lib/auth.ts
+++ lib/auth.ts
@@ -2,6 +2,7 @@ import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 
 import { prisma } from "./db";
+import { nextCookies } from "better-auth/next-js";
 
 export const auth = betterAuth({
   database: prismaAdapter(prisma, {
@@ -13,4 +14,5 @@ export const auth = betterAuth({
       clientSecret: process.env.GITHUB_CLIENT_SECRET as string,
     },
   },
+  plugins: [nextCookies()],
 });
