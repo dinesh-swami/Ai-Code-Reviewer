0774d98780f6a201f7484c75f4cf2cf4ea1be9a1
Dinesh Swami <dinesh907989@gmail.com>
2026-06-24 00:53:44 +0530

    final authentication complete and also profile sesction added

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 // "use client";
 // import { authClient } from "@/lib/auth-client";
+import { UserMenuWithSession } from "@/features/auth/components/user-menu";
 import Image from "next/image";
 
 export default function Home() {
@@ -7,6 +8,7 @@ export default function Home() {
   // console.log(data?.user);
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
+      < UserMenuWithSession variant="compact" />
       <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
         <Image
           className="dark:invert"
